e0f3c3a356afd1689c905eb9abd0045c355001ec
todaxyz <dotelabruh11@gmail.com>
2025-12-08 20:54:11 +0700

    Add files via upload

---
+++ Skema Database Sistem/Skema Database SDG 8 081225.sql
@@ -0,0 +1,180 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sistem_kerja
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `customer_service`
+--
+
+LOCK TABLES `customer_service` WRITE;
+/*!40000 ALTER TABLE `customer_service` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer_service` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `cv`
+--
+
+LOCK TABLES `cv` WRITE;
+/*!40000 ALTER TABLE `cv` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cv` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `komunikasi`
+--
+
+LOCK TABLES `komunikasi` WRITE;
+/*!40000 ALTER TABLE `komunikasi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `komunikasi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `lamaran`
+--
+
+LOCK TABLES `lamaran` WRITE;
+/*!40000 ALTER TABLE `lamaran` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lamaran` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `layanan_pelatihan`
+--
+
+LOCK TABLES `layanan_pelatihan` WRITE;
+/*!40000 ALTER TABLE `layanan_pelatihan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `layanan_pelatihan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `lowongan`
+--
+
+LOCK TABLES `lowongan` WRITE;
+/*!40000 ALTER TABLE `lowongan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lowongan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `pekerja`
+--
+
+LOCK TABLES `pekerja` WRITE;
+/*!40000 ALTER TABLE `pekerja` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pekerja` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `pengalaman`
+--
+
+LOCK TABLES `pengalaman` WRITE;
+/*!40000 ALTER TABLE `pengalaman` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pengalaman` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `pengumuman`
+--
+
+LOCK TABLES `pengumuman` WRITE;
+/*!40000 ALTER TABLE `pengumuman` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pengumuman` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `pengusaha`
+--
+
+LOCK TABLES `pengusaha` WRITE;
+/*!40000 ALTER TABLE `pengusaha` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pengusaha` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `perusahaan`
+--
+
+LOCK TABLES `perusahaan` WRITE;
+/*!40000 ALTER TABLE `perusahaan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `perusahaan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `pihak_ketiga`
+--
+
+LOCK TABLES `pihak_ketiga` WRITE;
+/*!40000 ALTER TABLE `pihak_ketiga` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pihak_ketiga` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `promosi`
+--
+
+LOCK TABLES `promosi` WRITE;
+/*!40000 ALTER TABLE `promosi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `promosi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `sertifikasi`
+--
+
+LOCK TABLES `sertifikasi` WRITE;
+/*!40000 ALTER TABLE `sertifikasi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sertifikasi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `ulasan`
+--
+
+LOCK TABLES `ulasan` WRITE;
+/*!40000 ALTER TABLE `ulasan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ulasan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `ulasan_perusahaan`
+--
+
+LOCK TABLES `ulasan_perusahaan` WRITE;
+/*!40000 ALTER TABLE `ulasan_perusahaan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ulasan_perusahaan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-08 20:42:48
